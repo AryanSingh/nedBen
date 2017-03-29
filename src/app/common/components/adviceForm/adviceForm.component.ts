6b7e7db021b9afd0abe43c30e4b27c4d2681bdec
aryan singh <aryan.singh@grexter.in>
2017-03-29 18:07:37 +0530

    advice form component being done

--- src/app/common/components/adviceForm/adviceForm.component.ts
+++ src/app/common/components/adviceForm/adviceForm.component.ts
@@ -1,17 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { THREAD_MANAGEMENT_URLS, ENV}  from '../../../app.constant';
+import { THREAD_MANAGEMENT_URLS, ADVICE_MANAGEMENT_URLS, GENERICS, ENV}  from '../../../app.constant';
 import { $WebSocket } from 'angular2-websocket/angular2-websocket';
-import { SessionService } from '../common/sessionService';
+import { SessionService } from '../../sessionService';
+import { IOffer, ICategory } from '../../interfaces/offer';
 
 @Component({
-  selector: 'footer',
-  templateUrl: './footer.html',
-  styleUrls: ['./footer.scss']
+  selector: 'adviceForm',
+  templateUrl: './adviceForm.html',
+  styleUrls: ['./adviceForm.scss']
 })
-export class FooterComponent implements OnInit {
+export class AdviceFormComponent implements OnInit {
     private ENV:any;
-    private THREAD_MANAGEMENT_URLS:any;
+	private ADVICE_MANAGEMENT_URLS:any;
+	private GENERICS:any;
+	private error:boolean;
+	private errorMessage:string;
+	private advice:any;
+	private loggedUserId:number;
+	private forumId:number;
+	private forumCategories:{};
+	private tag:string;
 
     constructor(
         private http: Http,
@@ -19,9 +28,97 @@ export class FooterComponent implements OnInit {
     ){};
 
     ngOnInit(): void {
-        this.ENV = ENV;
-        this.THREAD_MANAGEMENT_URLS = THREAD_MANAGEMENT_URLS;
+		this.ENV = ENV;
+		this.ADVICE_MANAGEMENT_URLS = ADVICE_MANAGEMENT_URLS;
+		this.GENERICS = GENERICS;
+		// this.loggedUserId= this.session.getUserData() ? this.session.getUserData().id : 0;
+		// 	if(this.loggedUserId=== 0){
+		// 						this.$rootScope.$broadcast('Auth-openModal');
+		// }
+		this.getForumCategories();
+		// if($stateParams.forumId){
+		// 	this.forumId=$stateParams.forumId;   
+		// 	this.getForumFullDeatil();
+		// } 
     }
+
+    getForumFullDeatil(){
+						this.$http.post(this.ENV.apiBasePath + this.ADVICE_MANAGEMENT_URLS.GET_DETAIL,
+												{idForum:this.forumId,idLoggedUser:this.loggedUserId}
+										,{headers: {'Content-Type': 'application/json'}})
+										.success(
+												(data, status) => {
+													this.advice=data;
+													this.tag=this.advice.tags.toString();
+												}
+										)
+										.error(
+												(error) => {
+														this.error = true;
+														this.errorMessage = error.errorMessage;
+												}
+										);
+				}
+
+				getForumCategories() {
+						this.$http.get(this.ENV.apiBasePath + this.GENERICS.GET_FORUM_CATEGORY_INSLIST)
+								.success(
+										(data, status) => {        
+												this.forumCategories = data;
+										}
+								).error((err)=>{
+									toastr.error('<p><strong>'+err.errorMessage+'</strong></p>');
+								});
+				}
+
+				insertAdvice() {
+						if (this.session.getUserData()) {
+								try{
+										this.advice.category= JSON.parse(this.advice.category);
+								}
+								catch (e){
+										// console.log(e);
+								}
+								this.advice.dateIns = new Date();
+								this.advice.tags = this.tag.split(',');
+								this.advice.user = this.session.getUserData();
+										if(this.forumId){
+														this.$http.put(this.ENV.apiBasePath + this.ADVICE_MANAGEMENT_URLS.UPDATE_FORUM, this.advice
+														,{headers: {'Content-Type': 'application/json'}})
+														.success(
+																(data, status) => {
+																	toastr.success('Modifiche risparmiare con successo');
+																	this.$location.path('/forumdetail/'+this.forumId);
+																}
+														)
+														.error(
+																(error) => {
+																		this.error = true;
+																		this.errorMessage = error.errorMessage;
+																}
+														);
+
+												
+										} else {
+														this.$http.put(this.ENV.apiBasePath + this.ADVICE_MANAGEMENT_URLS.INSERT, this.advice
+														,{headers: {'Content-Type': 'application/json'}})
+														.success(
+																(data, status) => {
+																	toastr.success('Nuova domanda creare con successo');
+																	this.$location.path('/forumdetail/'+data['id']);
+																}
+														)
+														.error(
+																(error) => {
+																		this.error = true;
+																		this.errorMessage = error.errorMessage;
+																}
+														);
+								}  
+						} else {
+								this.$rootScope.$broadcast('Auth-openModal');
+						}
+				}
 
     
 }
